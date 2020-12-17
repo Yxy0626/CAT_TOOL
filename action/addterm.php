d0326f79dc7fa52e58830d68086ef69cd0ad0540
Yxy0626 <73863695+Yxy0626@users.noreply.github.com>
2020-12-18 01:33:03 +0800

    Add files via upload

---
+++ action/addterm.php
@@ -0,0 +1,38 @@
+<?php
+
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'cat_database2');
+
+$conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+if(!$conn)
+{
+  echo "连接失败了！！";
+}
+
+mysqli_select_db($conn,"cat_database2"); //连接数据库
+
+mysqli_query($conn,"set names utf8"); //防止出现中文乱码的情况
+
+mysqli_query($conn,"set names utf8"); //防止出现中文乱码的情况
+ 
+$sql = "INSERT INTO `cat_database2`.`terms`(name,ST,TT) VALUES ('{$_POST["user"]}','{$_POST["ST"]}','{$_POST["TT"]}')";
+
+$insert = mysqli_query($conn,$sql);  
+ 
+if(!$insert)  
+    {  
+        echo "无法新增术语信息: ".mysqli_error($conn);  
+    }  
+    else  
+    {  
+		header("location: ../pages/TB.php");
+    }  
+ 
+ 
+mysqli_close($conn);
+
+?>
