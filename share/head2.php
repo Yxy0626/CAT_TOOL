d55c9a089324e71ffea6b293b4e39c006e3b695f
Yxy0626 <73863695+Yxy0626@users.noreply.github.com>
2021-01-19 03:09:44 +0800

    Add files via upload

---
+++ share/head2.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<!-- 新 Bootstrap 核心 CSS 文件 -->
+<link href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+ 
+<!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+<script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
+ 
+<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+<script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<script src="../assets/js/jquery.jeditable.min.js"></script>
+<script src="../assets/js/jquery.jeditable.autogrow.min.js"></script>
+<script src="../assets/js/jquery.autogrowtextarea.js"></script>
+<title>翻译页面</title>
+
+<div class="row">
+<div class="col-md-12">
+    <div class="container-fluid">
+        <h1 align="center" >翻译界面</h1>
+	<div>
